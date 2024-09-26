ac69edb768f765a2292b98d5ee7f50c8b0e69c35
tahmid-saj <tahmid.saj@gmail.com>
2024-09-25 20:43:28 -0400

    dashboard routes init

--- models/dashboard.go
+++ models/dashboard.go
@@ -1,2 +1,45 @@
 package models
 
+type Log struct {
+	Timestamp string `json:"timestamp"`
+	HostName string `json:"hostName"`
+	Method string	`json:"method"`
+	Path string `json:"path"`
+	Ok bool `json:"ok"`
+	Error bool `json:"error"`
+	Description string `json:"description"`
+}
+
+type DashboardInput struct {
+	StartTime string `json:"startTime"`
+	EndTime string `json:"endTime"`
+	HostName string `json:"hostName"`
+	Path string `json:"path"`
+	Ok bool `json:"ok"`
+}
+
+type DashboardInputByBucket struct {
+	StartTime string `json:"startTime"`
+	EndTime string `json:"endTime"`
+	HostName string `json:"hostName"`
+	Path string `json:"path"`
+	Ok bool `json:"ok"`
+	BucketName string `json:"bucketName"`
+}
+
+// dashboard
+func GetDashboardLogs(dashboardInput *DashboardInput) (*Response, error) {
+	
+}
+
+func GetDashboardLogsAggregated(dashboardInput *DashboardInput) (*Response, error) {
+
+}
+
+func GetDashboardLogsByBucket(dashboardInputByBucket *DashboardInputByBucket) (*Response, error) {
+
+}
+
+func GetDashboardLogsAggregatedByBucket(dashboardInputByBucket *DashboardInputByBucket) (*Response, error) {
+
+}
